f07d077581c4599ccc37fbe123cca40b33893287
Vitor Venturini <vfventurini@gmail.com>
2024-05-02 15:11:31 -0300

    criar projeto react

--- frontend-react-ts-tailwind/src/App.tsx
+++ frontend-react-ts-tailwind/src/App.tsx
@@ -6,9 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
