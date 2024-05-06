eeeb5b8323455672f1e649c167688e88d7a13aff
VitorVenturini <vfventurini@gmail.com>
2024-05-06 13:11:07 -0300

    criar projeto vite ts

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Button } from "@/components/ui/button"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,10 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <Button>Funfiona porra</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
