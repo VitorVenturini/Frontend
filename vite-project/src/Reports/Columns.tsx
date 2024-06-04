2d119d30f12e4082d4f97fc5eeb9bdd3305d0a81
Vitor Venturini <vfventurini@gmail.com>
2024-06-04 16:25:47 -0300

    arumar account e columms

--- vite-project/src/Reports/Columns.tsx
+++ vite-project/src/Reports/Columns.tsx
@@ -3,71 +3,15 @@ import UpdateUsers from "@/components/UpdateUsers";
 import DeleteUsers from "@/components/DeleteUsers";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react"
-import { Trash2 } from "lucide-react";
-import { Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogClose,
-} from "@/components/ui/dialog";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export interface User {
   id: string;
   name: string;
   guid: string;
   email: string;
   sip: string;
-  // Adicione aqui outros campos se necessário
 }
 
-// const deleteUsers = async (id: string) => {
-//   console.log(`id: ${id}`);
-//   const formData = {
-//     id: id,
-//   };
-//   try {
-//     const response = await fetch("https://meet.wecom.com.br/api/deleteUser", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "x-auth": localStorage.getItem("token") || "",
-//       },
-//       body: JSON.stringify(formData),
-//     });
-//     const data: User[] = await response.json();
-//     setUsers(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "id",
@@ -113,5 +57,4 @@ export const columns: ColumnDef<User>[] = [
       );
     },
   },
-  // Adicione aqui outras colunas conforme necessário
 ];
--- vite-project/src/pages/admin/Account.tsx
+++ vite-project/src/pages/admin/Account.tsx
@@ -1,29 +1,6 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useToast } from "@/components/ui/use-toast";
-import React, { useEffect, useState, ChangeEvent } from "react";
-import { Value } from "@radix-ui/react-select";
-import TableUser from "@/components/TableUser";
-import { Loader2 } from "lucide-react";
-import CardCreateAccount from "@/components/CardCreateAccount";
-import DemoPage from "@/Reports/page";
+import { useEffect, useState } from "react";
 import { DataTable } from "@/Reports/data-table";
 import { User, columns } from "@/Reports/Columns";
 
@@ -54,16 +31,9 @@ export default function Account() {
 
   return (
     <div className="px-2 flex flex-col gap-4 justify-center mx-[250px]">
-      <div className="flex justify-between gap-5">
-        <Input className="w-[500px]" placeholder="Nome de usuário..." />
-        <CardCreateAccount />
-      </div>
-      <Card className="w-full min-h-[700px]">
-        <ScrollArea className="h-[700px]">
-          <TableUser />
-        </ScrollArea>
-      </Card>
-      <DataTable columns={columns} data={users} />
+      <ScrollArea className="h-[500px]">
+        <DataTable columns={columns} data={users} />
+      </ScrollArea>
     </div>
   );
 }
