25de074273a0821482cd7d020d5cc26a883af802
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-07-14 16:07:12 -0300

    ajuste nova url wss

--- vite-project/src/components/websocket/useWebSocket.tsx
+++ vite-project/src/components/websocket/useWebSocket.tsx
@@ -25,9 +25,9 @@ const useWebSocket = (
     const currentUrl = window.location.hostname;
     let wsUrl = "";
     if (window.location.protocol === "http:") {
-      wsUrl = "wss://meet.wecom.com.br:443?token=" + token;
+      wsUrl = "wss://meet.wecom.com.br:443/ws?token=" + token;
     } else {
-      wsUrl = "wss://" + currentUrl + ":443?token=" + token;
+      wsUrl = "wss://" + currentUrl + ":443/ws?token=" + token;
     }
 
     function connect() {
