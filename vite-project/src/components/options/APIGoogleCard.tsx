79983702b43cc035d91d19789981d7c386885cf9
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-07-16 17:04:49 -0300

    ModalGoogleApiKey

--- vite-project/src/components/options/APIGoogleCard.tsx
+++ vite-project/src/components/options/APIGoogleCard.tsx
@@ -1,10 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
+import { useState } from "react";
 
+export default function APIGoogleCard() {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [key, setKey] = useState("")
 
-
-export default function APIGoogleCard(){
-
-    return(
-        <h2>EM DESENVOLVIMENTO</h2>
-    )
-
+    const handleGoogleKey = () =>{
+        
+    }
+  return (
+    <Card className="w-[50%] h-fit">
+      <CardHeader>
+        <CardTitle>API Google</CardTitle>
+        <CardDescription>Chave da API do google</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="w-full flex flex-col gap-5">
+          <div className="flex items-center justify-between gap-3 w-full">
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              Chave
+            </h4>
+            <Input placeholder="Chave" className="w-full" />
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <Button>Salvar</Button>
+      </CardFooter>
+    </Card>
+  );
 }
