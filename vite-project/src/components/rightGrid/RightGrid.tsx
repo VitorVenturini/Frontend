504d3700769555f5bd2ae932582b5c2e3e6a9488
Vitor Venturini <vfventurini@gmail.com>
2024-07-22 14:28:00 -0300

    atualização layout camera rightbottomopt

--- vite-project/src/components/rightGrid/RightGrid.tsx
+++ vite-project/src/components/rightGrid/RightGrid.tsx
@@ -60,7 +60,7 @@ export default function RightGrid({
     }
   });
   return (
-    <Card className="  flex flex-col gap-1 items-center xl:h-[600px] xl:w-[400px] 2xl:w-[520px] 2xl:h-[790px]">
+    <Card className="  flex flex-col gap-1 items-center lg:h-[630px] lg:w-[400px] xl:h-[700px] xl:w-[400px] 2xl:w-[520px] 2xl:h-[790px]">
       {selectedUser && (
         <div className="w-full">
           <div className="flex-grow w-full p-1">
