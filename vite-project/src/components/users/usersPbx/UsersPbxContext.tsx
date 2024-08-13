8525fc662b7257aa8fa8f29b63a90e0c103ce99e
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-08-13 13:18:32 -0300

    status usuário

--- vite-project/src/components/users/usersPbx/UsersPbxContext.tsx
+++ vite-project/src/components/users/usersPbx/UsersPbxContext.tsx
@@ -1,37 +1,36 @@
-
 import React, { createContext, useState, useContext, ReactNode } from "react";
 
 export interface DeviceInterface {
-    hw: string;
-    text?: string;
-    app?: string;
-    tls?: boolean;
-    trusted?: boolean;
-  }
-  
-  export interface UserPbxInterface {
-    id: number;
-    cn: string;
-    guid: string;
-    email: string;
-    sip: string;
-    e164: string;
-    devices: DeviceInterface[]; 
-    password?: string;
-    createdAt?: string; // o ? significa que o valor nao precisa ser presente, se for nulo nao tem problema
-    updatedAt?: string;
-    type?: string;
-    status?: string;
-    note?: string;
-    color?: string;
-  }
+  hw: string;
+  text?: string;
+  app?: string;
+  tls?: boolean;
+  trusted?: boolean;
+}
+
+export interface UserPbxInterface {
+  id: number;
+  cn: string;
+  guid: string;
+  email: string;
+  sip: string;
+  e164: string;
+  devices: DeviceInterface[];
+  password?: string;
+  createdAt?: string; // o ? significa que o valor nao precisa ser presente, se for nulo nao tem problema
+  updatedAt?: string;
+  type?: string;
+  status?: string;
+  note?: string;
+  color?: string;
+}
 
 interface UserPbxContextType {
   usersPbx: UserPbxInterface[];
   setUsersPbx: React.Dispatch<React.SetStateAction<UserPbxInterface[]>>;
   addUsersPbx: (user: UserPbxInterface) => void;
   updateUserPbx: (user: UserPbxInterface) => void;
-  updateUserPbxStauts: (guid: string, status: string) => void;
+  updateUserPbxStauts: (guid: string, status: string, note?: string) => void;
   deleteUserPbx: (id: number) => void;
 }
 
@@ -50,9 +49,11 @@ export const UserPbxProvider = ({ children }: { children: ReactNode }) => {
       )
     );
   };
-  const updateUserPbxStauts = (guid: string, status: string) => {
+  const updateUserPbxStauts = (guid: string, status: string, note?: string) => {
     setUsersPbx((prevUsers) =>
-      prevUsers.map((user) => (user.guid === guid ? { ...user, status } : user))
+      prevUsers.map((user) =>
+        user.guid === guid ? { ...user, status, note } : user
+      )
     );
   };
   const deleteUserPbx = (id: number) => {
@@ -66,8 +67,7 @@ export const UserPbxProvider = ({ children }: { children: ReactNode }) => {
         addUsersPbx,
         updateUserPbx,
         updateUserPbxStauts,
-        deleteUserPbx
-        
+        deleteUserPbx,
       }}
     >
       {children}
