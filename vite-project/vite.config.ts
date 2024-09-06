bb2877c24951b88ea98279521c81cdbac64305b0
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-09-06 16:25:51 -0300

    implementações para clientes futuros

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, '/home/wecom/wecom.com.br.key')),
+  //     cert: fs.readFileSync(path.resolve(__dirname, '/home/wecom/wecom.com.br.pem')),
+  //   },
+  //   host: '0.0.0.0', // para permitir acesso externo, se necessário
+  //   port: 443, // você pode definir a porta desejada
+  //   hmr: {
+  //     overlay: false, // Desabilita o overlay de erros
+  //   },
+  // },
 })
