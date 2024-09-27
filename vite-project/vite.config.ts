fb2f489f25faa717ea6d0ca2857ecb161da2fd53
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-09-27 14:39:11 -0300

    Vite Config + Npm Modules

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import fs from "fs";
+import os from "os"; 
+
+// Verifica se o hostname é diferente de 'localhost'
+const isLocalhost = os.hostname() === "localhost" || os.hostname().startsWith("127.0.0.1");
 
 export default defineConfig({
   plugins: [react()],
@@ -10,15 +14,23 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync(path.resolve(__dirname, '/home/wecom/wecom.com.br.key')),
-  //     cert: fs.readFileSync(path.resolve(__dirname, '/home/wecom/wecom.com.br.pem')),
-  //   },
-  //   host: '0.0.0.0', // para permitir acesso externo, se necessário
-  //   port: 4343, // você pode definir a porta desejada
-  //   hmr: {
-  //     overlay: false, // Desabilita o overlay de erros
-  //   },
-  // },
-})
+  ...(isLocalhost
+    ? {} // Se for localhost, não aplica HTTPS
+    : {
+        server: {
+          https: {
+            key: fs.readFileSync(
+              path.resolve(__dirname, "/home/wecom/wecom.com.br.key")
+            ),
+            cert: fs.readFileSync(
+              path.resolve(__dirname, "/home/wecom/wecom.com.br.pem")
+            ),
+          },
+          host: "0.0.0.0", // para permitir acesso externo
+          port: 4343, // define a porta desejada
+          hmr: {
+            overlay: false, // Desabilita o overlay de erros
+          },
+        },
+      }),
+});
