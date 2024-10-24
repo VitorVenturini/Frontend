cc2e21bb17a2cb2b2dbe1329587913709f4ec229
Vitor Venturini <vfventurini@gmail.com>
2024-10-24 14:46:05 -0300

    fix safari

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import Loader from "./components/Loader"; // Supondo que você tenha um componente Loader
 
 function Main() {
