237de89f4373ad9d7858bac764c7bfdd242732cf
Vitor Venturini <vfventurini@gmail.com>
2024-11-11 10:40:35 -0300

    Botão de aparencia bonico e fix de logo

--- vite-project/src/components/options/ApparenceCard.tsx
+++ vite-project/src/components/options/ApparenceCard.tsx
@@ -76,6 +76,47 @@ export default function ApparenceCard() {
       reader.readAsDataURL(file);
     }
   };
+  const handleUpdateLogo = async () => {
+    if (!fileContent) {
+      toast({
+        variant: "destructive",
+        description: "Por favor, selecione um arquivo antes de salvar.",
+      });
+      return;
+    }
+    setIsCreating(true);
+    try {
+      const formData = new FormData();
+      formData.append("file", fileContent, "logomarca.png");
+      const response = await fetch(`${host}/api/uploadFiles`, {
+        method: "POST",
+        headers: {
+          "x-auth": account.accessToken || "",
+        },
+        body: formData,
+      });
+      if (response.ok) {
+        toast({
+          description: "Imagem atualizada com sucesso!",
+        });
+      } else {
+        const data = await response.json();
+        console.error("Erro ao enviar a imagem", data);
+        toast({
+          variant: "destructive",
+          description: "Erro ao enviar a imagem.",
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao enviar a imagem", error);
+      toast({
+        variant: "destructive",
+        description: "Erro ao enviar a imagem.",
+      });
+    } finally {
+      setIsCreating(false);
+    }
+  };
 
   const handleUpdateColor = async () => {
     setUpdateColor(true);
@@ -88,8 +129,15 @@ export default function ApparenceCard() {
         entry: "theme",
       });
       setTheme(selectedColor as any);
+      toast({
+        description: "Tema atualizado com sucesso !",
+      });
     } catch (error) {
       console.error("Error updating theme color:", error);
+      toast({
+        variant: "destructive",
+        description: "Erro ao atualizar a cor do tema.",
+      });
     } finally {
       setUpdateColor(false);
     }
@@ -118,6 +166,7 @@ export default function ApparenceCard() {
                 onChange={handleLogoChange}
               />
             </div>
+            <div className="flex items-center justify-between">
             <Card className="max-h-[500px] max-w-[500px]">
               <CardHeader>
                 <CardTitle className="flex justify-center">preview</CardTitle>
@@ -128,32 +177,50 @@ export default function ApparenceCard() {
                 </div>
               </CardContent>
             </Card>
+            <Button
+              onClick={handleUpdateLogo}
+              disabled={updateColor}
+              className="mt-4"
+            >
+              {updateColor ? "Saving..." : "Salvar imagem"}
+            </Button>
+            </div>
+            
             <div className="flex justify-between w-full items-center">
               <Label> Escolha uma cor</Label>
 
               <Button
+                variant="outline"
+                className=" gap-2 flex items-center"
                 onClick={() => {
                   setSelectedColor("blue");
                   setTheme("blue");
                 }}
               >
                 Azul
+                <div className=" bg-blue-400 w-[20px] h-[20px]" />
               </Button>
               <Button
+                variant="outline"
+                 className=" gap-2 flex items-center"
                 onClick={() => {
                   setSelectedColor("red");
                   setTheme("red");
                 }}
               >
                 Vermelho
+                <div className=" bg-red-400 w-[20px] h-[20px]"/>
               </Button>
               <Button
+                variant="outline"
+                 className=" gap-2 flex items-center"
                 onClick={() => {
                   setSelectedColor("zinc");
                   setTheme("zinc");
                 }}
               >
                 Zinco
+                <div className=" bg-zinc-400 w-[20px] h-[20px]" />
               </Button>
             </div>
             <Button
@@ -165,18 +232,7 @@ export default function ApparenceCard() {
             </Button>
           </div>
         </CardContent>
-        <CardFooter className="justify-end">
-          <Button disabled={isLoading}>
-            {isLoading ? (
-              <>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Salvando
-              </>
-            ) : (
-              "Salvar"
-            )}
-          </Button>
-        </CardFooter>
+
       </Card>
     </div>
   );
