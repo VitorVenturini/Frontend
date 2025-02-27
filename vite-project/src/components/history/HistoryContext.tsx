6c0992e1fa8c5f2296986d4b97d23b87ed252669
Vitor Venturini <vfventurini@gmail.com>
2025-02-27 13:52:20 -0300

    pesquisa de buttons e sensor funcioanando

--- vite-project/src/components/history/HistoryContext.tsx
+++ vite-project/src/components/history/HistoryContext.tsx
@@ -59,31 +59,6 @@ export const HistoryProvider = ({ children }: { children: ReactNode }) => {
   const [history, setHistoryState] = useState<HistoryInterface[]>([]);
   const [historyComplete, setHistoryComplete] = useState<boolean>(false);
 
-  // const addHistory = (newHistory: HistoryInterface) => {
-  //   // Verifica se já existe uma entrada com o mesmo id
-  //   const isDuplicate = history.some((hist) => hist.id === newHistory.id);
-
-  //   console.log("histContext newHistory", newHistory);
-  //   //console.log("histContext history", history);
-  //   useEffect(() => {
-  //     console.log("History atualizado:", history);
-  //   }, [history]);
-
-  //   if (!isDuplicate) {
-  //     // Se não for duplicado, adiciona ao histórico
-  //     console.log("//////////////  NEW History /////////////", newHistory);
-  //     setHistoryState((prevHistory) => [newHistory, ...prevHistory]);
-  //   } else {
-  //     // Se for duplicado, atualiza o item correspondente
-  //     setHistoryState((prevHistory) =>
-  //       prevHistory.map((hist) =>
-  //         hist.id === newHistory.id ? { ...hist, ...newHistory } : hist
-  //       )
-  //     );
-  //     console.log("//////////////  UPDATE History /////////////", newHistory);
-  //     console.log("///////////// histContext history /////////////", history);
-  //   }
-  // };
   const addHistory = (newHistory: HistoryInterface) => {
     setHistoryState((prevHistory) => {
       const existingIndex = prevHistory.findIndex((hist) => hist.id === newHistory.id);
--- vite-project/src/components/sensor/ResponsiveIcon.tsx
+++ vite-project/src/components/sensor/ResponsiveIcon.tsx
@@ -16,18 +16,8 @@ export default function ResponsiveIcon({
       return <Cloudy className="p-0.5" />;
     case "pressure":
       return <CircleGauge className="p-0.5" />;
-    // case "wind_direction":
-    //   return <Compass className="p-0.5" />;
     default:
       return "";
   }
 
-  // if (oldValue < newValue) {
-  //   return <CircleArrowUp size={20} color="green" />;
-  // } else if (oldValue > newValue) {
-  //   return <CircleArrowDown size={20} color="red" />;
-  // }
-  //else {
-  //     return <Check size={20} color="green" />;
-  //   }
 }
